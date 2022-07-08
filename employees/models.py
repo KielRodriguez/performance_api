0c9aaa84885d38d7cbdbd9640df447c7b1e29112
KielRodriguez <kr.rdz.20@gmail.com>
2022-07-08 00:19:13 -0500

    create employee app

--- employees/models.py
+++ employees/models.py
@@ -5,3 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, unique=True)
+
+    def __str__(self):
+        return '{first_name} {last_name}'.format(first_name=self.user.first_name, last_name=self.user.last_name)
+
+    class Meta:
+        verbose_name_plural = "Employees"
