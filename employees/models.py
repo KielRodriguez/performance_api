61fd91dcaa4c0a45db7159a7db000d215f4a398c
KielRodriguez <kr.rdz.20@gmail.com>
2022-07-09 00:31:06 -0500

    upload serialized to employee class

--- employees/models.py
+++ employees/models.py
@@ -3,11 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, unique=True)
-
+class Employee(User):
     def __str__(self):
-        return '{first_name} {last_name}'.format(first_name=self.user.first_name, last_name=self.user.last_name)
+        return '{first_name} {last_name}'.format(first_name=self.first_name, last_name=self.last_name)
 
     class Meta:
         verbose_name_plural = "Employees"
