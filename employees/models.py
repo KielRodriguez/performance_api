e6200eba7378aa8c94343733a034e59d84a0988d
KielRodriguez <kr.rdz.20@gmail.com>
2022-07-09 15:35:28 -0500

    add review model

--- employees/models.py
+++ employees/models.py
@@ -1,6 +1,8 @@
+from pyexpat import model
 from django.db import models
 
 from django.contrib.auth.models import User
+from pkg_resources import require
 
 # Create your models here.
 class Employee(User):
@@ -9,3 +11,12 @@ class Employee(User):
 
     class Meta:
         verbose_name_plural = "Employees"
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, related_name="reviewer")
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, related_name="employee")
+    rating = models.FloatField(max_length=5.0, null=False, default=0)
+    feedback = models.TextField(max_length=300, null=True)
+
+    def __str__(self):
+        return 'rating: {}'.format(str(self.rating))
