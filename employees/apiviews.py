af86fda3369fcf50b616696bf6e32b4dacf075d1
KielRodriguez <kr.rdz.20@gmail.com>
2022-07-09 11:04:54 -0500

    implement update and save functions to employees

--- employees/apiviews.py
+++ employees/apiviews.py
@@ -1,22 +1,36 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Employee
-from .serializers import EmployeeSerializer
+from .serializers import EmployeeSerializer, EmployeeCreateSerializer
 
-class EmployeeList(APIView):
-    def get(self, request):
-        employees = Employee.objects.all()
-        data = EmployeeSerializer(employees, many=True).data
-        return Response(data)
+# class EmployeeList(APIView):
+#     def get(self, request):
+#         employees = Employee.objects.all()
+#         data = EmployeeSerializer(employees, many=True).data
+#         return Response(data)
 
-class EmployeeDetail(APIView):
-    def get(self, request, id):
-        employee = get_object_or_404(Employee, pk=id)
-        data = EmployeeSerializer(employee, many=False).data
-        return Response(data)
+# class EmployeeDetail(APIView):
+#     def get(self, request, id):
+#         employee = get_object_or_404(Employee, pk=id)
+#         data = EmployeeSerializer(employee, many=False).data
+#         return Response(data)
 
+class EmployeeList(generics.ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmployeeDetail(generics.RetrieveDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmployeeSave(generics.CreateAPIView):
+    serializer_class = EmployeeCreateSerializer
+
+class EmployeeUpdate(generics.UpdateAPIView):
+    serializer_class = EmployeeCreateSerializer
 
 
--- employees/urls.py
+++ employees/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from employees.apiviews import EmployeeList, EmployeeDetail
+from employees.apiviews import EmployeeList, EmployeeDetail, EmployeeSave, EmployeeUpdate
 
 urlpatterns = [
     path('employees/', EmployeeList.as_view(), name='employee_list'),
     path('employees/<int:id>', EmployeeDetail.as_view(), name='employee_detail'),
+    path('employees/', EmployeeSave.as_view, name='employee_save' ),
+    path('employees/<int:id>', EmployeeUpdate.as_view, name='employee_update'),
 ]
